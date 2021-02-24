d3d7284b83b86a7f739c830b280800dd64d6f783
Мухамеджанова Эльмира <timra.work@gmail.com>
2021-02-24 14:17:49 +0500

    feat(game.tsx): get winner and show status game

--- src/containers/pages/Game.tsx
+++ src/containers/pages/Game.tsx
@@ -8,32 +8,74 @@ interface IProps {
 // Square
 export const Square: FC<IProps> = ({value, setActive}) => {
   return (
-    <button className="square" onClick={() => setActive()}>
+    <button className="square" onClick={setActive}>
       {value}
     </button>
   );
 };
 
+const getWinner = (squares: Array<string | number | null>) => {
+  const winnerLines = [
+    // horizontal indexes
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // vertical indexes
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonal indexes
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < winnerLines.length; i++) {
+    const [firstIdx, secondIdx, thirdIdx] = winnerLines[i];
+    // prettier-ignore
+    if (squares[firstIdx] &&
+        squares[firstIdx] === squares[secondIdx] &&
+        squares[firstIdx] === squares[thirdIdx])
+      {
+        return squares[firstIdx];
+      }
+  }
+  return null;
+};
+
+const getStatus = (winner: any, xIsNext: any) => {
+  console.log('winner', winner);
+  let status = winner ? 'Winner: ' + winner : 'Next: ' + (xIsNext ? 'O' : 'X');
+  return status;
+};
+
 // Board - first child
 export const Board = () => {
-  const status = 'Next player: X';
   const [squares, setSquares] = useState<Array<string>>(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [status, setStatus] = useState<string | number | null>(null);
 
   const handleClick = (i: number) => {
     const array: Array<string> = squares.slice();
-    array[i] = 'X';
+    // ignore click if has winner or if the cell is already full(repeat click)
+    if (/* (getWinner(array) || */ squares[i]) {
+      return;
+    }
+    array[i] = xIsNext ? 'X' : 'O';
     setSquares(array);
+    setXIsNext(!xIsNext);
+
+    setStatus(getStatus(getWinner(array), xIsNext));
   };
 
   return (
-    <div>
+    <>
       <div className="status">{status}</div>
       <div className="board">
         {squares.map((el, idx) => {
           return <Square key={idx} value={el} setActive={() => handleClick(idx)} />;
         })}
       </div>
-    </div>
+    </>
   );
 };
 
