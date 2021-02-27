6303dbb1ba7b3aea3e2be6c65bb09a53acac7743
Мухамеджанова Эльмира <timra.work@gmail.com>
2021-02-28 00:19:23 +0500

    fix(btnfullscreen.tsx): correct types to moz, webkit, ms browsers

--- src/containers/parts/BtnFullScreen.tsx
+++ src/containers/parts/BtnFullScreen.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import FullscreenIcon from '@material-ui/icons/Fullscreen';
+interface FsDocument extends HTMLElement {
+  msRequestFullscreen?: () => void;
+  webkitRequestFullScreen?: () => void;
+  mozRequestFullScreen?: () => void;
+}
 
 export function BtnFullScreen() {
   const FullscreenIconClickHandler = () => {
-    const target = document.querySelector<HTMLElement>('.game');
+    const target = document.querySelector<FsDocument>('.game');
     if (target && target.requestFullscreen) target.requestFullscreen();
-    // mozilla proposal
-    // if (target && target.webkitRequestFullScreen) target.webkitRequestFullScreen();
-    // // Webkit (works in Safari and Chrome Canary)
-    // if (target && target.mozRequestFullScreen) target.mozRequestFullScreen();
+    if (target && target.webkitRequestFullScreen) target.webkitRequestFullScreen();
+    if (target && target.msRequestFullscreen) target.msRequestFullscreen();
   };
 
   return (
