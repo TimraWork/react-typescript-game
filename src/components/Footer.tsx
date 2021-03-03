1ea14cc35ecc90f35442337799eb9b0d63d4f0c5
Мухамеджанова Эльмира <timra.work@gmail.com>
2021-03-03 22:34:21 +0500

    feat: add telegram link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,15 @@
 import logoSrc from '../assets/img/rs_school_js.svg';
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import TelegramIcon from '@material-ui/icons/Telegram';
 
 export const Footer = () => (
   <footer>
     <IconButton color="primary" href="https://github.com/TimraWork" target="_blank">
       <GitHubIcon />
+    </IconButton>
+    <IconButton color="primary" href="https://telegram.me/timra_work" target="_blank">
+      <TelegramIcon />
     </IconButton>
     <div className="copyright">&copy; 2021</div>
     <a href={'https://rs.school/react/'} className="footer-logo" target="_blank">
